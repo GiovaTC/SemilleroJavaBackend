274d08156710db87c09a5989865a848ed6e1ed75
GiovaTC <90740161+GiovaTC@users.noreply.github.com>
2023-10-17 11:08:54 -0500

    Add files via upload

---
+++ Sesion06/src/main/java/org/bedu/java/backend/sesion06/Sesion06Application.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.sesion06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion06Application.class, args);
+	}
+
+}
---
+++ Sesion06/src/test/java/org/bedu/java/backend/sesion06/Sesion06ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.java.backend.sesion06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sesion06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
